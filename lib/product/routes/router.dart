5c4d560a0794a7946074db45fa52bebedab060c0
Osman Demiröz <osmanndemirozz@gmail.com>
2024-10-25 18:51:44 +0300

    unless features deleted

--- lib/product/routes/router.dart
+++ lib/product/routes/router.dart
@@ -1,13 +1,9 @@
 // ignore_for_file: public_member_api_docs
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:haber_uygulamasi/feature/view/home_view.dart';
 import 'package:haber_uygulamasi/feature/view/news_detail_view.dart';
 import 'package:haber_uygulamasi/feature/view/news_view.dart';
-import 'package:haber_uygulamasi/feature/view/profile_view.dart';
 import 'package:haber_uygulamasi/product/model/article.dart';
-
 part 'router.gr.dart';
 
 @AutoRouterConfig(
@@ -19,14 +15,8 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(
-          page: HomeRoute.page,
-        ),
-        AutoRoute(
-          page: NewsRoute.page,
           initial: true,
-        ),
-        AutoRoute(
-          page: ProfileRoute.page,
+          page: NewsRoute.page,
         ),
         AutoRoute(
           page: NewsDetailRoute.page,
--- lib/product/routes/router.gr.dart
+++ lib/product/routes/router.gr.dart
@@ -1,32 +1,10 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-
 // **************************************************************************
 // AutoRouterGenerator
 // **************************************************************************
-
 // ignore_for_file: type=lint
 // coverage:ignore-file
-
 part of 'router.dart';
-
-/// generated route for
-/// [HomePage]
-class HomeRoute extends PageRouteInfo<void> {
-  const HomeRoute({List<PageRouteInfo>? children})
-      : super(
-          HomeRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'HomeRoute';
-
-  static PageInfo page = PageInfo(
-    name,
-    builder: (data) {
-      return const HomePage();
-    },
-  );
-}
 
 /// generated route for
 /// [NewsDetailPage]
@@ -89,25 +67,6 @@ class NewsRoute extends PageRouteInfo<void> {
     name,
     builder: (data) {
       return const NewsPage();
-    },
-  );
-}
-
-/// generated route for
-/// [ProfilePage]
-class ProfileRoute extends PageRouteInfo<void> {
-  const ProfileRoute({List<PageRouteInfo>? children})
-      : super(
-          ProfileRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'ProfileRoute';
-
-  static PageInfo page = PageInfo(
-    name,
-    builder: (data) {
-      return const ProfilePage();
     },
   );
 }
